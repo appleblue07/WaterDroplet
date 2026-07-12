913fb2717ef997ba0f51dbd6d51bfcdddcd50ca2
blueapple07 <07jhgreen@gmail.com>
2026-07-12 23:24:32 +0900

    플레이어 움직임 추가 (부자연스러움)

--- scr/scripts.js
+++ scr/scripts.js
@@ -1,3 +1,4 @@
+
 const startScreen = document.getElementById("startScreen");
 const gameScreen = document.getElementById("gameScreen");
 const playBtn = document.getElementById("playBtn");
@@ -7,5 +8,46 @@ playBtn.addEventListener("click", () => {
     startScreen.style.display = "none";
     gameScreen.style.display = "flex";
 });
+
+
+/** 플레이어 이동*/
+const player = document.getElementById("player");
+let playerX = window.innerWidth / 2;
+
+let leftPressed = false;
+let rightPressed = false;
+
+document.addEventListener("keydown", (event) => {
+
+    if(event.key == "a"){
+        leftPressed = true;
+    }
+
+    if(event.key == "d"){
+        rightPressed = true;
+    }
+});
+
+function update(){
+
+    if(leftPressed){
+        playerX -= 30;
+    }
+
+    if(rightPressed){
+        playerX += 30;
+    }
+
+    player.style.left = playerX + "px";
+    leftPressed = false;
+    rightPressed = false;
+    requestAnimationFrame(update);
+}
+
+update();
+
+
+
+
 
 console.log("연결 완");
